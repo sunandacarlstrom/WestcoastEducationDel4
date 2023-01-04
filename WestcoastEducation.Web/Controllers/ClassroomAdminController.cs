73bc9ee2b1cc0ebbe24f894c26fbb1aafceeab34
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-01-04 16:00:10 +0100

    Listar alla kurser i en tabell i vyn ClassroomAdmin

--- WestcoastEducation.Web/Controllers/ClassroomAdminController.cs
+++ WestcoastEducation.Web/Controllers/ClassroomAdminController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WestcoastEducation.Web.Data;
 
 namespace WestcoastEducation.Web.Controllers
 {
     [Route("classroomadmin")]
     public class ClassroomAdminController : Controller
     {
-        public IActionResult Index()
+        private readonly WestcoastEducationContext _context; 
+        public ClassroomAdminController(WestcoastEducationContext context)
         {
-            return View("Index");
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Använder context för att koppla vyn till databasen
+            var classrooms = await _context.Classrooms.ToListAsync();
+            return View("Index", classrooms);
         }
     }
 }
