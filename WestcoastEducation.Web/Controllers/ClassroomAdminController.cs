09f99ec8499bef9b5747d875720b3f20f0d95999
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-01-05 14:26:02 +0100

    Lagt till felhantering

--- WestcoastEducation.Web/Controllers/ClassroomAdminController.cs
+++ WestcoastEducation.Web/Controllers/ClassroomAdminController.cs
@@ -16,9 +16,22 @@ namespace WestcoastEducation.Web.Controllers
 
         public async Task<IActionResult> Index()
         {
-            // Använder context för att koppla vyn till databasen
-            var classrooms = await _context.Classrooms.ToListAsync();
-            return View("Index", classrooms);
+            try
+            {
+                // använder context för att koppla vyn till databasen
+                var classrooms = await _context.Classrooms.ToListAsync();
+                return View("Index", classrooms);
+            }
+            catch (Exception ex)
+            {
+                var error = new ErrorModel
+                {
+                    ErrorTitle = "Ett fel har inträffat vid inhämtning av alla kurser",
+                    ErrorMessage = ex.Message
+                };
+
+                return View("_Error", error);
+            }
         }
 
         [HttpGet("create")]
@@ -32,7 +45,40 @@ namespace WestcoastEducation.Web.Controllers
         [HttpPost("create")]
         public async Task<IActionResult> Create(Classroom classroom)
         {
-            // lägg upp en kurs i minnet
+            try
+            {
+                // söker efter en kurstitel lika med det som kommer in i mitt anrop 
+                var exists = _context.Classrooms.SingleOrDefaultAsync(
+                    c => c.Title.Trim().ToUpper() == classroom.Title.Trim().ToUpper());
+
+                // kontrollerar om denna titeln redan existerar
+                if (exists is not null)
+                {
+                    var error = new ErrorModel
+                    {
+                        ErrorTitle = "Ett fel har inträffat när kursen skulle sparas",
+                        ErrorMessage = $"En kurs med titeln {classroom.Title} finns redan i systemet"
+                    };
+
+                    //skicka tillbaka en vy som visar information gällande felet 
+                    return View("_Error", error);
+                }
+            }
+            // Ett annat fel har inträffat som vi inte har räknat med...
+            catch (Exception ex)
+            {
+                var error = new ErrorModel
+                {
+                    ErrorTitle = "Ett fel har inträffat när kursen skulle sparas",
+                    ErrorMessage = ex.Message
+                };
+
+                return View("_Error", error);
+            }
+
+            // Om allt går bra, inga fel inträffar...
+
+            // lägg upp kursen i minnet
             await _context.Classrooms.AddAsync(classroom);
             // spara ner i databas
             await _context.SaveChangesAsync();
@@ -43,51 +89,98 @@ namespace WestcoastEducation.Web.Controllers
         [HttpGet("edit/{classroomId}")]
         public async Task<IActionResult> Edit(int classroomId)
         {
-            // får tillbaka en kurs och skicka till en vy
-            // här vill jag alltså få tag i en kurs med Id som är lika med det som kommer in mitt metodanrop
-            var classroom = await _context.Classrooms.SingleOrDefaultAsync(c => c.ClassroomId == classroomId);
+            try
+            {
+                // får tillbaka en kurs och skicka till en vy
+                // här vill jag alltså få tag i en kurs med Id som är lika med det som kommer in mitt metodanrop
+                var classroom = await _context.Classrooms.SingleOrDefaultAsync(c => c.ClassroomId == classroomId);
 
-            if (classroom is not null) return View("Edit", classroom);
-            return Content("Det gick fel...");
+                if (classroom is not null) return View("Edit", classroom);
+
+                var error = new ErrorModel
+                {
+                    ErrorTitle = "Ett fel har inträffat när en kurs skulle hämtas för redigering",
+                    ErrorMessage = $"Hittar ingen kurs med id {classroomId}"
+                };
+
+                return View("_Error", error);
+            }
+            catch (Exception ex)
+            {
+                var error = new ErrorModel
+                {
+                    ErrorTitle = "Ett fel har inträffat när en kurs skulle hämtas för redigering",
+                    ErrorMessage = ex.Message
+                };
+
+                return View("_Error", error);
+            }
         }
 
         [HttpPost("edit/{classroomId}")]
         public async Task<IActionResult> Edit(int classroomId, Classroom classroom)
         {
-            // vara säker på att kursen jag redigerar/uppdaterar verkligen finns i Changetracking listan
-            var classroomToUpdate = _context.Classrooms.SingleOrDefault(c => c.ClassroomId == classroomId);
+            try
+            {
+                // vara säker på att kursen jag redigerar/uppdaterar verkligen finns i Changetracking listan
+                var classroomToUpdate = _context.Classrooms.SingleOrDefault(c => c.ClassroomId == classroomId);
 
-            if (classroomToUpdate is null) return RedirectToAction(nameof(Index));
+                if (classroomToUpdate is null) return RedirectToAction(nameof(Index));
 
-            classroomToUpdate.Name = classroom.Name;
-            classroomToUpdate.Title = classroom.Title;
-            classroomToUpdate.Start = classroom.Start;
-            classroomToUpdate.End = classroom.End;
+                classroomToUpdate.Name = classroom.Name;
+                classroomToUpdate.Title = classroom.Title;
+                classroomToUpdate.Start = classroom.Start;
+                classroomToUpdate.End = classroom.End;
 
-            //uppdatera en kurs via ef 
-            _context.Classrooms.Update(classroomToUpdate);
+                //uppdatera en kurs via ef 
+                _context.Classrooms.Update(classroomToUpdate);
 
-            // spara ner i databas (alla ändringar på en o samma gång med _context)
-            await _context.SaveChangesAsync();
+                // spara ner i databas (alla ändringar på en o samma gång med _context)
+                await _context.SaveChangesAsync();
 
-            return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch (Exception ex)
+            {
+                var error = new ErrorModel
+                {
+                    ErrorTitle = "Ett fel har inträffat när redigering av kursen skulle sparas",
+                    ErrorMessage = ex.Message
+                };
+
+                return View("_Error", error);
+            }
         }
 
         [Route("delete/{classroomId}")]
         public async Task<IActionResult> Delete(int classroomId)
         {
-            //hämta in kursen som jag vill radera 
-            var classroomToDelete = await _context.Classrooms.SingleOrDefaultAsync(c => c.ClassroomId == classroomId);
+            try
+            {
+                //hämta in kursen som jag vill radera 
+                var classroomToDelete = await _context.Classrooms.SingleOrDefaultAsync(c => c.ClassroomId == classroomId);
 
-            if (classroomToDelete is null) return RedirectToAction(nameof(Index));
+                if (classroomToDelete is null) return RedirectToAction(nameof(Index));
 
-            //radera en kurs direkt 
-            _context.Classrooms.Remove(classroomToDelete);
+                //radera en kurs direkt 
+                _context.Classrooms.Remove(classroomToDelete);
 
-            // spara ner i databas (alla ändringar på en o samma gång med _context)
-            await _context.SaveChangesAsync();
+                // spara ner i databas (alla ändringar på en o samma gång med _context)
+                await _context.SaveChangesAsync();
 
-            return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                var error = new ErrorModel
+                {
+                    ErrorTitle = "Ett fel har inträffat när kursen skulle raderas",
+                    ErrorMessage = ex.Message
+                };
+
+                return View("_Error", error);
+            }
         }
     }
 }
